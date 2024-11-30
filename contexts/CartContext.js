d31148cb79e1f31b82f90112ad52f61f5886015a
miguelfcostaa <miguelcosta220@hotmail.com>
2024-11-30 17:26:25 +0000

    contextos

---
+++ contexts/CartContext.js
@@ -0,0 +1,44 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+    const [cart, setCart] = useState([]);
+    const [cartCount, setCartCount] = useState(0);
+    const [isAdded, setIsAdded] = useState(false);
+
+
+    const findItemInCart = (productId) => {
+        return cart.find((item) => item.id === productId);
+    };
+
+    const addToCart = (item) => {
+        setCart((prevCart) => {
+            const itemInCart = prevCart.find((cartItem) => cartItem.id === item.id);
+            if (itemInCart) {
+                return prevCart.map((cartItem) =>
+                    cartItem.id === item.id
+                        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                        : cartItem
+                );
+            }
+            return [...prevCart, { ...item, quantity: 1 }];
+        });
+    };
+    
+    const removeFromCart = (productId) => {
+        setCart((prevCart) => prevCart.filter((item) => item.id !== productId));
+    };
+
+    const clearCart = () => {
+        setCart([]);
+    };
+
+    return (
+        <CartContext.Provider value={{ setCart, cart, isAdded, setIsAdded, cartCount, setCartCount, addToCart, removeFromCart, clearCart, findItemInCart }}>
+            {children}
+        </CartContext.Provider>
+    );
+};
+
+export const useCart = () => useContext(CartContext);
---
+++ contexts/CoinContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const CoinContext = createContext();
+
+export const CoinProvider = ({ children }) => {
+    const [coins, setCoins] = useState(1200);
+
+    const addCoin = (value) => {
+        setCoins(coins + value);
+    }
+
+    const removeCoin = (value) => {
+        setCoins(coins - value);
+    }
+
+    return (
+        <CoinContext.Provider value={{ coins, setCoins, removeCoin, addCoin }}>
+            {children}
+        </CoinContext.Provider>
+    );
+};
+
+
+export const useCoin = () => useContext(CoinContext);
